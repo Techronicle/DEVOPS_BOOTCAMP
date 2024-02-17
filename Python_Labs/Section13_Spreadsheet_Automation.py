c16026e3dc0259010515e1e5421857fd950fa0f5
Balvish <roybalvish@gmail.com>
2024-02-17 11:09:07 +0000

    Spreadsheet_Automation Exercise

---
+++ Python_Labs/Section13_Spreadsheet_Automation.py
@@ -0,0 +1,39 @@
+import openpyxl 
+
+#open target excel file 
+
+inv_file = openpyxl.load_workbook("inventory.xlsx") 
+
+#retrieve sheet1 from the excel file
+
+product_list = inv_file["Sheet1"] 
+
+#Get the number of product per supplier#  
+
+# (1) Starting with an empty dictionary 
+
+products_per_supplier = {}  
+
+# (2) Looping through the excel rows to fetch required values 
+#       Skipping the headers and going all the way down to the last row
+#       Iterate through the sheet to get all the supplier Names, Column 4
+
+for product_row in range(2, product_list.max_row + 1):
+   supplier_name = product_list.cell(product_row, 4).value
+   #print(supplier_name)
+
+   if supplier_name in products_per_supplier:
+       current_number = products_per_supplier[supplier_name] 
+       products_per_supplier[supplier_name] = current_number + 1 
+
+   else:
+       print("Adding a new supplier")
+       products_per_supplier[supplier_name] = 1 
+    
+
+print(products_per_supplier)
+
+
+
+
+
